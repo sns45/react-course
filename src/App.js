a216f8799edbf2276839981e8cb5e9cc454704cb
Shantanu Sharma <sns45@njit.edu>
2018-04-18 19:57:32 -0400

    conditional show/hide

--- src/App.js
+++ src/App.js
@@ -17,7 +17,8 @@ class App extends Component {
         name: "Ashley",
         age: 26
       },
-    ]
+    ],
+    showPersons: true
   }
 
   switchnameHandler = (newName) => {
@@ -41,28 +42,61 @@ class App extends Component {
 
   changename = (event) => {
       this.switchnameHandler(event.target.value);
+      
+  }
+
+  toggleview = () => {
+    const show = this.state.showPersons
+    this.setState({
+      showPersons: !show
+    })
   }
 
   render() {
+    const style = {
+      backgroundColor: 'white',
+      font: 'inherit',
+      border: '1px solid blue',
+      padding: '8px',
+      cursor: 'pointer'
+    };
+
+    let persons = null;
+
+
+    if (this.state.showPersons) {
+      persons = (
+        <div>
+          <Person
+            name={this.state.person[0].name}
+            age={this.state.person[0].age}
+            // avoid using this
+            click={() => this.switchnameHandler('Sean Bean')} />
+          <Person
+            name={this.state.person[1].name}
+            age={this.state.person[1].age}
+            changename={this.changename}
+          >Likes guitar</Person>
+
+          <Person
+            name={this.state.person[2].name}
+            age={this.state.person[2].age} />
+        </div>
+      )
+    }
+
     return (
       <div className="App">
         <h1>This is a react app</h1>
-        <Person
-          name={this.state.person[0].name}
-          age={this.state.person[0].age}
-          // avoid using this
-          click={ () => this.switchnameHandler('Sean Bean')} />          
-
-        <button onClick={this.switchnameHandler.bind(this, 'Sean Bean')}>Switch Name</button>
-        <Person
-          name={this.state.person[1].name}
-          age={this.state.person[1].age}  
-          changename = {this.changename}
-          > Likes guitar </Person>
-
-        <Person
-          name={this.state.person[2].name}
-          age={this.state.person[2].age} />
+        <button 
+          style = {style}
+          onClick={this.toggleview}>
+          Show/hide</button>
+          <button 
+          style = {style}
+          onClick={this.switchnameHandler.bind(this, 'Sean Bean')}>
+          Switch Name</button>
+       {persons}
       </div>
     );
   }
